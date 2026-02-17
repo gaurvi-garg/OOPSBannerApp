d8c7fbaf930e17bdba7631a31ca501254b4769eb
Gaurvi Garg <garggaurvi0@gmail.com>
2026-02-17 21:58:29 +0530

    UC2: Display OOPS as ASCII banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,11 @@
 public class OOPSBannerApp {
-        public static void main(String[] args) {
-        // Print "OOPS" to console
-        System.out.println("OOPS");
+    public static void main(String[] args) {
+        System.out.println("  *** " + " " + "   *** " + " " + " ***** " + " " + " ***** ");
+        System.out.println(" ** **" + " " + " **  **" + " " + " ** **" + " " + " ** ");
+        System.out.println("**   **" + " " + "**   **" + " " + "** **" + " " + " ** ");
+        System.out.println("**   **" + " " + "**   **" + " " + "***** " + " " + " ***** ");
+        System.out.println("**   **" + " " + "**   **" + " " + "** " + " " + "      **");
+        System.out.println(" ** **" + " " + "  ** **" + " " + " ** " + " " + " ** * *");
+        System.out.println("  *** " + " " + "   *** " + " " + " ** " + " " + "  *** ");
     }
 }
